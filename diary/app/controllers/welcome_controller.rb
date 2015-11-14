329a2c6105bdf814a004a085e03d0ffa3bcfe661
Naty <matykowska.natalia@gmail.com>
2015-11-14 18:37:45 +0100

    add "entries" controller, add "Entry" model

--- diary/app/controllers/welcome_controller.rb
+++ diary/app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+	  @entries = Entry.order(date: :desc)
+	  @websites = [
+  					["http://railsgirls.com", "Rails Girls"],
+  					["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+  					["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+  ]
   end
   
   def index2
--- diary/config/routes.rb
+++ diary/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-    get 'welcome/index2'
+  get 'welcome/index2'
+  resources "entries"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
