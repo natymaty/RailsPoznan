d70ae6c6dc362b2f92dd485bc90922754cd522ae
Naty <matykowska.natalia@gmail.com>
2015-11-18 16:43:04 +0100

    database migration from sqlite3 to psql

--- diary/db/schema.rb
+++ diary/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151114142716) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string   "title"
     t.date     "date"
