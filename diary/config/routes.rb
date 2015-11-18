81e4c599625eb0e94c5e1a98b141c15a83f3611c
Naty <matykowska.natalia@gmail.com>
2015-11-18 11:54:09 +0100

    add view for new - entry adding form

--- diary/config/routes.rb
+++ diary/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/index2'
   resources "entries"
+  create 'welcome/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
